197e98825a7fc20a5958ffcc568dfac4ff621fe5
Victor Soto <sotovictor@outlook.fr>
2025-05-11 19:30:44 +0200

    Updates

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,17 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true, 
+  swcMinify: true,
+  experimental: {
+    appDir: true,
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': '.',
+    };
+    return config;
+  },
 };
+
+module.exports = nextConfig;
