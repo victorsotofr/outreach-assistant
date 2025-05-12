1288dc86c6db716f7fba0380defc3525282a08fb
Victor Soto <sotovictor@outlook.fr>
2025-05-12 18:47:40 +0200

    Updates

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,10 @@
 const path = require('path');
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   webpack: (config) => {
     config.resolve.alias['@'] = path.resolve(__dirname);
     return config;
