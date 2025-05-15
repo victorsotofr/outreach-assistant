17de69c56a7a3bdc60cb5ce645703b63491b7866
Victor Soto <sotovictor@outlook.fr>
2025-05-15 12:23:17 +0200

    Updates

--- frontend/components/ContactPreviewDialog.tsx
+++ frontend/components/ContactPreviewDialog.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Button } from "@/components/ui/button";
-import { toast } from "react-hot-toast";
+import { toast } from "sonner";
 
 interface ContactPreviewDialogProps {
   data: any[];
